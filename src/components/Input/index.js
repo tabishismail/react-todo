ccb942a2b96639dba395512d0249958852af9167
tabish1235 <tabish554@gmail.com>
2021-09-16 01:09:34 +0500

    add and delete button functions done

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,9 +1,9 @@
 import "./css/index.css"
 
 
-function Input({type,placeholder}){
+function Input({type,placeholder,value,onChange}){
     return (
-        <input className="input" type={type} placeholder={placeholder} />
+        <input onChange={onChange} value={value} className="input" type={type} placeholder={placeholder} />
     )
 }
 export default Input;
--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,19 +1,40 @@
 import { Button, Input } from '..';
 import './css/index.css'
-
+import React from 'react';
 
 
 function Container() {
-
-    return (
-      <div className="board" >
-        <h1 className="heading">Todo List</h1>
-        <div className="list">
-          <Input type="text" placeholder="Enter Your Desire ToDo"/>
-          <Button myClass="myAddBtn">Add ToDo</Button>
-        </div>
-      </div>
-  
-    );
+  const [value, setValue] = React.useState("");
+  const [todo, setTodo] = React.useState([]);
+  // console.log("todos ==>>", todo);
+  const addTodo = () => {
+    let arr = [...todo];
+    arr.push(value);
+    setTodo(arr);
+    setValue("")
   }
-  export default Container;
+  const deleteTodo = (i) => {
+    let arr = [...todo];
+    arr.splice(i,1)
+    setTodo(arr);
+  }
+
+  return (
+    <div className="board" >
+      <h1 className="heading">Todo List</h1>
+      <Input onChange={(e) => setValue(e.target.value)} value={value} type="text" placeholder="Enter Your Desire ToDo" />
+      <Button onClick={addTodo} myClass="myAddBtn">Add ToDo</Button>
+      <div className="list">
+        <ul>
+          {todo.map((v,i)=>{
+            return(
+              <li key={i}><Input value={v}/> <Button myClass="myEditBtn">Edit</Button><Button onClick={()=>deleteTodo(i)} myClass="myDeleteBtn">Delete</Button></li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+
+  );
+}
+export default Container;
